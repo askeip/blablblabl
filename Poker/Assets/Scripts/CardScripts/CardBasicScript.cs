340abb67bcc8f155a936154843c14d8dd251b4dd
askeip <paparazzi730@gmail.com>
2015-07-28 14:17:54 +0600

    Scripts replaced, CardScript splited

---
+++ Poker/Assets/Scripts/CardScripts/CardBasicScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Linq;
+using System;
+
+public class CardBasicScript : MonoBehaviour 
+{
+	public int Rank;
+	
+	public Suits Suit;
+	
+	public void SetCard(string cardName)
+	{
+		Rank = RankToInt(cardName[0]);
+		Suit = SuitToSuits(cardName[1]);
+		ShowCard (cardName);
+		//string name = "5S";
+	}
+
+	public virtual void ShowCard(string cardName)
+	{
+	}
+	
+	private Suits SuitToSuits(char suit)
+	{
+		switch (suit) 
+		{
+		case 'S':return Suits.Spikes;
+		case 'H':return Suits.Hearts;
+		case 'D':return Suits.Diamonds;
+		default:return Suits.Clubs;
+		}
+	}
+	private int RankToInt(char rank)
+	{
+		switch (rank) 
+		{
+		case 'T': return 10;
+		case 'J': return 11;
+		case 'Q': return 12;
+		case 'K': return 13;
+		case 'A': return 14;
+		default: return int.Parse(rank.ToString());
+		}
+	}
+}
---
+++ Poker/Assets/Scripts/CardScripts/CardScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Linq;
+using System;
+
+public class CardScript : CardBasicScript
+{
+	public override void ShowCard (string cardName)
+	{
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Cards/" + cardName);
+		Resources.UnloadUnusedAssets ();
+	}
+}
