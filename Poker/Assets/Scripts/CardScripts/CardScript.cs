717fa0e457c17bea6a674d757e4d27800c011e08
askeip <paparazzi730@gmail.com>
2015-07-31 18:10:21 +0500

    GetComponent removed

--- Poker/Assets/Scripts/CardScripts/CardScript.cs
+++ Poker/Assets/Scripts/CardScripts/CardScript.cs
@@ -15,13 +15,13 @@ public class CardScript : CardBasicScript
 
 	public override void ShowCard ()
 	{
-		this.gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("OtherCards/" + ((Card.Rank - 2) + 13 * (int)Card.Suit));
+		cardSprite = Resources.Load<Sprite> ("OtherCards/" + ((Card.Rank - 2) + 13 * (int)Card.Suit));
 		Resources.UnloadUnusedAssets ();
 	}
 
 	public override void HideCard()
 	{
-		this.gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite> ("OtherCards/" + "flipside");
+		cardSprite = Resources.Load<Sprite> ("OtherCards/" + "flipside");
 		Resources.UnloadUnusedAssets ();
 	}
 }
